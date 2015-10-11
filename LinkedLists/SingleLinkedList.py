33fad1c62dc6d9ebe2ea0d72e7258102318e9e9a
mgiota <giota85@gmail.com>
2015-10-11 23:02:55 +0200

    Find merge point of 2 linked lists

--- LinkedLists/SingleLinkedList.py
+++ LinkedLists/SingleLinkedList.py
@@ -194,4 +194,20 @@ class SingleLinkedList():
         		else:
             			slow = slow.next
             			fast = fast.next.next
-           
+	
+	def FindMergeNode(headA, headB):
+		currentA = headA
+		currentB = headB
+
+		while currentA != currentB:
+			if currentA.next == None:
+				currentA = headB
+			else:
+				currentA = currentA.next
+			
+			if currentB.next == None:
+				currentB = headA
+			else:
+				currentB = currentB.next
+		
+		return currentA.data          
