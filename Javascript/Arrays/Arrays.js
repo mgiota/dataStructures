b3bf6135fc47aa46f02a703877a4682ccff9bcd0
mgiota <giota85@gmail.com>
2016-02-24 21:57:26 +0100

    Get first n elements of an array

--- Javascript/Arrays/Arrays.js
+++ Javascript/Arrays/Arrays.js
@@ -2,4 +2,23 @@ exports = module.exports = {};
 
 exports.is_array = function(arr) {
 	return toString.call(arr) === '[object Array]';
-}
+};
+
+exports.clone = function(arr) {
+  return arr.slice(0);
+};
+
+exports.getFirstNElements = function(arr, n) {
+  if (arr == null) {
+    return void 0;
+  }
+  if (n == null) {
+    return arr[0];
+  }
+
+  if (n < 0) {
+    return [];
+  }
+
+  return arr.slice(0, n);
+};
--- Javascript/Arrays/test/ArraysSpec.js
+++ Javascript/Arrays/test/ArraysSpec.js
@@ -1,6 +1,18 @@
 var expect = require('chai').expect;
 var arrays = require('../Arrays.js');
 
+function arraysEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for(var i = arr1.length; i > 0; i--) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 describe('is_array', function() {
   it('#should return true if input is an array', function() {
     var input = [1, 2, 3];
@@ -12,5 +24,58 @@ describe('is_array', function() {
     var input = 'test';
     var result = arrays.is_array(input);
     expect(result).to.equal(false);
+  });
+});
+
+describe('clone', function() {
+  it('should clone an array', function() {
+    var arr = [1, 2, 3];
+
+    var clone = arrays.clone(arr);
+
+    expect(clone.length).to.equal(3);
+    expect(clone[0]).to.equal(1);
+    
+  });
+});
+
+describe('getFirstNElements', function() {
+  it('should return 1st element if no n parameter is passed', function() {
+    var arr = [1, 2, 3];
+    expect(arrays.getFirstNElements(arr)).to.equal(1);
+  });
+
+  it('should return empty array if we pass an empty array', function() {
+    var arr1 = [];
+    var arr2 = arrays.getFirstNElements(arr1, 3);
+    var result = arraysEqual(arr1, arr2);
+    expect(result).to.equal(true);
+  });
+
+  it('should return first n elements', function() {
+    var arr1 = [7, 9, 0, -2],
+        expected = [7, 9, 0],
+        actual = arrays.getFirstNElements(arr1, 3),
+        result = arraysEqual(expected, actual);
+
+    expect(result).to.equal(true);
+  });
+
+  it('should return array if n parameter bigger than array length', function() {
+    var arr1 = [7, 9, 0, -2],
+        expected = arr1,
+        actual = arrays.getFirstNElements(arr1, 6),
+        result = arraysEqual(expected, actual);
+
+    expect(result).to.equal(true);
+  });
+
+  it('should return an empty array if a negative parameter is passed', function() {
+    var arr1 = [7, 9, 0, -2],
+        expected = [],
+        actual = arrays.getFirstNElements(arr1, -3),
+        result = arraysEqual(expected, actual);
+
+    expect(result).to.equal(true);
   });
 });
